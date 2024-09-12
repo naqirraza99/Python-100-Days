1605794d4c949d7a0821c9a145a3e735c41130a5
naqirraza99 <119874734+naqirraza99@users.noreply.github.com>
2024-09-11 22:32:41 -0700

    Add Lesson 1: Basic Printing Examples

---
+++ Day-1-Challenge/Lesson1 Printing.py
@@ -0,0 +1,17 @@
+ print('-------------Lesson 1 Day 1: "Printing"--------------------')
+ print()
+
+print("Hello,World!") #double quotation mark
+    print(5) #print number
+    print(6.78) #print decimal value
+    print('Naqi Raza') #single quotation mark
+    print('She said: "HEllo" Where are you!') # single quoation mark in " " statement
+    #print("She said: "Hello" Where are you")
+    print("She said: 'Hello' Where are you")
+    print("She said: \"Hello\" Where are you") #double quoation mark in " " statement
+
+    print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+    print("2. Knead the dough for 10 minutes.")
+    print("3. Add 3g of Salt.")
+    print("4. Leave to rise for 2 hours.")
+    print("5. Bake at 200 degrees C for 30 minutes.")
